98d62058e8fed65ce33b2380676f41a15bb87aa6
Andrei Robson da Silva <andreirobson@furb.br>
2023-04-16 18:43:38 -0300

    nota

---
+++ loops/src/Exercio2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Exercio2 {
+    public static void main(String[] args) {
+        Scanner tec = new Scanner(System.in);
+        int nota;
+
+        System.out.println("Digite uma nota entre 0 e 10: ");
+        nota = tec.nextInt();
+        while (nota < 0 || nota > 10) {
+            System.out.println("Nota invalida!");
+            System.out.println("Digite uma nota entre 0 e 10: ");
+            nota = tec.nextInt();
+        }
+    }
+}
