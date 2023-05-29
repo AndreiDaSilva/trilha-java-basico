9e67fd7d14ea9592c96cb24818bb007294441c7e
Andrei Robson <andreirobson@furb.br>
2023-05-28 21:53:57 -0300

    Classe No finalizado

--- Conhecendo-Java/Estrutura-Dados/src/com/aula02/listaEncadeadas/No.java
+++ Conhecendo-Java/Estrutura-Dados/src/com/aula02/listaEncadeadas/No.java
@@ -18,30 +18,18 @@ public class No<T> {
         this.proximoNo = proximoNo;
     }
 
-    /**
-     * @return the conteudo
-     */
     public T getConteudo() {
         return conteudo;
     }
 
-    /**
-     * @param conteudo the conteudo to set
-     */
     public void setConteudo(T conteudo) {
         this.conteudo = conteudo;
     }
 
-    /**
-     * @return the proximoNo
-     */
     public No getProximoNo() {
         return proximoNo;
     }
 
-    /**
-     * @param proximoNo the proximoNo to set
-     */
     public void setProximoNo(No proximoNo) {
         this.proximoNo = proximoNo;
     }
@@ -54,7 +42,7 @@ public class No<T> {
     public String toStringEncadeado() {
         String str = "No [conteudo=" + conteudo + "]";
 
-        if(proximoNo != null){
+        if (proximoNo != null) {
             str += "->" + proximoNo.toString();
         } else {
             str += "-> null";
